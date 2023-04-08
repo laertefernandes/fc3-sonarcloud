bc237d9c3d841ace6bb5ba0fa383537fa6f7927e
LaerteNF <laertenf@gmail.com>
2023-04-08 16:12:36 -0300

    bug: add correction test2

--- sum_test.go
+++ sum_test.go
@@ -1,10 +1,13 @@
 package main
 
-func TestSum(t *testing.T){
-	
-	result := sum(2,3)
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	result := sum(2, 3)
 
 	if result != 5 {
 		t.Error("The result must be 5")
 	}
+
 }
